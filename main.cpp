3cb46723aad89eff239342816e774ad74639d303
Mariya <varmar2000@yandex.ru>
2021-10-18 00:48:32 +0300

    77

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
     if (autotest() == 0) {
         CVector v1, v2;
         double a;
+        int q, w;
 
         cout << "Dlya prekrasheniya vvoda vvedite bukvu ili slovo \n" << endl;
 
@@ -33,6 +34,9 @@ int main() {
             if (cin.good()) v2.add(a);
         }
 
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
         cout << endl;
         cout << "v1 = " << v1 << endl;
         cout << "v2 = " << v2 << endl;
@@ -43,7 +47,41 @@ int main() {
         cout << "v1 - v2 = " << (v1 - v2) << endl;
         cout << "v1 * v2 = " << (v1 * v2) << endl;
 
+        cout << "Do you want to add coordinates ?  yes -0, no-1\n" << endl;
+        cin >> q;
+        if (q == 0) {
+            cout << " Wich vector do you want to change 1 or 2?" << endl;
+            cin >> w;
+            cout << "add coordinates\n" << endl;
+            if (w == 1) {
+                cin >> a;
+                v1.add(a);
+                while (cin.good()) { // Пока не встретилась ошибка чтения. Она произойдет когда введется отличный от double тип данных
+                    cin >> a;
+                    if (cin.good()) v1.add(a);
+                }
+                cout << "v1 = " << v1 << endl;
+                cout << endl;
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+            if (w == 2) {
 
+                cin >> a;
+                v2.add(a);
+                while (cin.good()) { // Пока не встретилась ошибка чтения. Она произойдет когда введется отличный от double тип данных
+                    cin >> a;
+                    if (cin.good()) v2.add(a);
+                }
+                cout << "v2 = " << v2 << endl;
+                cout << endl;
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            }
+        }
+        else {
+            return 0;
+        }
         return 0;
     }
     else {
